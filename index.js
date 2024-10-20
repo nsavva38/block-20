c41a67c7c5c0630d3e5ef010e47daa7fe222df2d
nsavva38 <nsavva38@gmail.com>
2024-10-20 12:01:38 -0400

    fixed sort 1 bugs

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ console.log(`test`);
 //43 minutes in, Buttons when clicked
 //58 minutes, 
 //how to remove item
+const inputNumberArray = [];
 
 const addNumberButton = document.querySelector(`#number-button`);
 addNumberButton.addEventListener(`click`, (event) => {
   event.preventDefault();
   console.log(`Clicked`);
-  const userInput = document.querySelector(`input`);
+  const userInput = document.querySelector(`#number`);
   console.log(`userInput.value: ${userInput.value}`);
 
   const numberOutput = document.querySelector(`#number-bank-output`);
 
   const numberLI = document.createElement(`li`)
-  numberLI.classList.add(`inputNumber`);
-
+  numberLI.id = `numberLI-${userInput.value}`;
+  // numberLI.classList.add(`inputNumber`);
   numberLI.innerText = userInput.value;
   numberOutput.append(numberLI);
 
@@ -23,125 +24,176 @@ addNumberButton.addEventListener(`click`, (event) => {
   const inputNumber = document.querySelectorAll(`.inputNumber`);
   const inputArray = [...inputNumber];
 
-  let inputNumberArray = inputArray.map((item) => {
-    return Number(item.textContent);
-  });
 
+  
+  inputNumberArray.push( {value: userInput.value, element: numberLI });
+  // let inputNumberArray = inputArray.map((item) => {
+  //   return Number(item.textContent);
+  // });
+
+  userInput.value = ``;
   console.log(`inputNumberArray: ${inputNumberArray}`);
+  console.log(inputNumberArray);
   console.log(``);
+
+
+
+
+
 
 
 
   
   const sortButtons = document.querySelector(`#sortButtons`);
 
-  sortButtons.addEventListener(`click`, (event) => {
-    numberLI.remove();
-    console.log(event);
+  // Clear any existing event listeners on the sortButtons element
+  sortButtons.replaceWith(sortButtons.cloneNode(true));
+  const newSortButtons = document.querySelector(`#sortButtons`);
+
+  // Attach the event listener once
+  newSortButtons.addEventListener(`click`, (event) => {
     if (event.target.tagName === `BUTTON`) {
       console.log(`A button was clicked`);
-   
-      if(event.target.innerText === `Sort 1`) {
-        // sort 1
-
-        // go to even pile
-        if(inputNumberArray[0] % 2 === 0) {
-
-          const numberEvenOutput = document.querySelector(`#evens-output`);
-
-          const numberEvenLI = document.createElement(`li`)
-          numberEvenLI.classList.add(`evenNumber`);
-        
-          numberEvenLI.innerText = inputNumberArray[0];
-          numberEvenOutput.append(numberEvenLI);
-
-
-        } else {
-
-          // go to odd pile
-          const numberOddOutput = document.querySelector(`#odds-output`);
-
-          const numberOddLI = document.createElement(`li`)
-          numberOddLI.classList.add(`oddNumber`);
-        
-          numberOddLI.innerText = inputNumberArray[0];
-          numberOddOutput.append(numberOddLI);
-
-        }
-
-        console.log(`before sort: ${inputNumberArray}`);
-
-        for (let i = 0; i < inputNumberArray.length; i++) {
-
-          if( (i + 1) === inputNumberArray.length) {
-            inputNumberArray.pop();
-          } else {
-            inputNumberArray[i] = inputNumberArray[i + 1];
-          }
-          inputNumberArray[i] = inputNumberArray[i + 1];
-
-          console.log(`after sort: ${inputNumberArray}`);
-          console.log(``);
-          
-
-        }
-
-      } else if (event.target.innerText === `Sort All`) {
-        // sort all
-
-        for (let i = 0; i < inputNumberArray.length; i++) {
-
-          if(inputNumberArray[0] % 2 === 0) {
-
-            const numberEvenOutput = document.querySelector(`#evens-output`);
   
-            const numberEvenLI = document.createElement(`li`)
-            numberEvenLI.classList.add(`evenNumber`);
-          
-            numberEvenLI.innerText = inputNumberArray[0];
-            numberEvenOutput.append(numberEvenLI);
-  
-  
-          } else {
-  
-            // go to odd pile
-            const numberOddOutput = document.querySelector(`#odds-output`);
-  
-            const numberOddLI = document.createElement(`li`)
-            numberOddLI.classList.add(`oddNumber`);
-          
-            numberOddLI.innerText = inputNumberArray[0];
-            numberOddOutput.append(numberOddLI);
-  
-          }
-  
-          console.log(`before sort: ${inputNumberArray}`);
+    // Check if there are elements left to sort
+    if (inputNumberArray.length > 0) {
+      // Get the first element from the array
+      const { value: currentNumber, element: currentElement } = inputNumberArray[0];
 
-          if( (i + 1) === inputNumberArray.length) {
-            inputNumberArray.pop();
-          } else {
-            inputNumberArray[i] = inputNumberArray[i + 1];
-          }
-          inputNumberArray[i] = inputNumberArray[i + 1];
-
-          
-          
-
-        }
-        console.log(`after sort: ${inputNumberArray}`);
-        console.log(``);
-
-
-
-
-        // to reset the array:
-        inputNumberArray = [];
-
+      if (currentNumber % 2 === 0) {
+        // Sort into even pile
+        const numberEvenOutput = document.querySelector('#evens-output');
+        const numberEvenLI = document.createElement('li');
+        numberEvenLI.classList.add('evenNumber');
+        numberEvenLI.innerText = currentNumber;
+        numberEvenOutput.append(numberEvenLI);
+      } else {
+        // Sort into odd pile
+        const numberOddOutput = document.querySelector('#odds-output');
+        const numberOddLI = document.createElement('li');
+        numberOddLI.classList.add('oddNumber');
+        numberOddLI.innerText = currentNumber;
+        numberOddOutput.append(numberOddLI);
       }
 
-    }
+      // Remove the sorted element from the input array
+      inputNumberArray.shift();
 
-    },{ once: true }); // The listener is removed after being called once),
+      // Remove the corresponding numberLI element from the DOM
+      currentElement.remove();
+
+      // Log the remaining elements in the array
+      console.log('Remaining elements in the array:', inputNumberArray.map(item => item.value));
+    } else {
+      console.log('No more elements to sort.');
+    }
+  }
+});
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  //,{ once: true }); // The listener is removed after being called once), put in between
+  //event listener closing brace and closing parenthesis
+
+//         console.log(`before sort: ${inputNumberArray}`);
+
+//         for (let i = 0; i < inputNumberArray.length; i++) {
+
+//           if( (i + 1) === inputNumberArray.length) {
+//             inputNumberArray.pop();
+//           } else {
+//             inputNumberArray[i] = inputNumberArray[i + 1];
+//           }
+//           inputNumberArray[i] = inputNumberArray[i + 1];
+
+//           console.log(`after sort: ${inputNumberArray}`);
+//           console.log(``);
+          
+
+//         }
+
+//       } 
+//         else if (event.target.innerText === `Sort All`) {
+//         // sort all
+
+//         for (let i = 0; i < inputNumberArray.length; i++) {
+
+//           if(inputNumberArray[i] % 2 === 0) {
+
+//             const numberEvenOutput = document.querySelector(`#evens-output`);
+  
+//             const numberEvenLI = document.createElement(`li`)
+//             numberEvenLI.classList.add(`evenNumber`);
+          
+//             numberEvenLI.innerText = inputNumberArray[i];
+//             numberEvenOutput.append(numberEvenLI);
+  
+  
+//           } else {
+  
+//             // go to odd pile
+//             const numberOddOutput = document.querySelector(`#odds-output`);
+  
+//             const numberOddLI = document.createElement(`li`)
+//             numberOddLI.classList.add(`oddNumber`);
+          
+//             numberOddLI.innerText = inputNumberArray[0];
+//             numberOddOutput.append(numberOddLI);
+  
+//           }
+  
+//           console.log(`before sort: ${inputNumberArray}`);
+
+//           if( (i + 1) === inputNumberArray.length) {
+//             inputNumberArray.pop();
+//           } else {
+//             inputNumberArray[i] = inputNumberArray[i + 1];
+//           }
+//           inputNumberArray[i] = inputNumberArray[i + 1];
+
+          
+          
+
+//         }
+//         console.log(`after sort: ${inputNumberArray}`);
+//         console.log(``);
+
+
+
+
+//         // to reset the array:
+//         inputNumberArray = [];
+
+//       }
+
+//     }
+
+//     },{ once: true }); // The listener is removed after being called once),
 
 
 });
